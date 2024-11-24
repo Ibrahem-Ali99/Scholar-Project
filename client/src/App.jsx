24659af89e6ff114d3ce7e25693ce27a86ea1288
mohamed-hassan-747 <s-mohamed-hassan@zewailcity.edu.eg>
2024-11-24 09:34:30 +0200

    Update the structure of the project.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import StudentDashboard from "./pages/StudentDashboard";
+import StudentDashboard from "./pages/StudentDashboard/StudentDashboard";
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
