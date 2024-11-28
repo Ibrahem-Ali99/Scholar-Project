d7f59a3db974e53ce9e112d7084e0b2360a02f38
Ibrahem-Ali99 <s-ibrahem.ali@zewailcity.edu.eg>
2024-11-28 20:28:15 +0200

    Changed the axios to fetch in Feedback.jsx to keep consistency

--- server/routes/feedback.py
+++ server/routes/feedback.py
@@ -5,17 +5,16 @@ feedback_bp = Blueprint('feedback', __name__)
 
 @feedback_bp.route('/ratings', methods=['GET'])
 def get_all_feedbacks():
-    feedbacks = CourseRating.query.all()  # Fetch all feedbacks from the CourseRating table
+    feedbacks = CourseRating.query.all()  
     feedback_list = []
     
-    # Loop through the feedbacks and extract necessary information
     for feedback in feedbacks:
         feedback_list.append({
             'rating_id': feedback.rating_id,
-            'student_name': feedback.student.name,  # Assuming 'name' is a column in the 'Student' table
+            'student_name': feedback.student.name,  
             'rating': feedback.rating,
             'comment': feedback.comment,
-            'course_name': feedback.course.course_name  # Assuming 'course_name' is a column in the 'Course' table
+            'course_name': feedback.course.course_name  
         })
     
     return jsonify(feedback_list)
