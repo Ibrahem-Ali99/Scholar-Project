5affde9499f1c49f1763331d5edba1ffdffe47aa
galalqassas <110430283+galalqassas@users.noreply.github.com>
2024-11-29 18:08:35 +0200

    Added teacher dashboard front end pages

---
+++ client/src/pages/students/Student.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Box, Typography } from "@mui/material";
+import { columns, rows } from "./data";
+import Header from "../../components/Header";
+
+const Student = () => {
+  return (
+    <Box>
+      <Header
+        title="Students"
+        subTitle="List of Student for Future Reference"
+      />
+
+      <Box sx={{ height: 650, width: "99%", mx: "auto" }}>
+        <DataGrid
+          slots={{
+            toolbar: GridToolbar,
+          }}
+          rows={rows}
+          // @ts-ignore
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Student;
