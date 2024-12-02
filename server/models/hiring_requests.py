6a5a4d1b95becc7467db936b118588df35fccb57
YusufBashaar <132370331+YusufBashaar@users.noreply.github.com>
2024-12-03 01:39:21 +0200

    Update hiring_requests.py

--- server/models/hiring_requests.py
+++ server/models/hiring_requests.py
@@ -2,10 +2,12 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.db import db
-class Session(db.Model):
-    __tablename__ = 'sessions'
-    session_id = db.Column(db.Integer, primary_key=True)
-    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.teacher_id'), nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.course_id'), nullable=False)
-    notification_id = db.Column(db.Integer, db.ForeignKey('notification.notification_id'), nullable=False)
-    session_date = db.Column(db.Date, nullable=False)
+
+class HiringRequest(db.Model):
+    __tablename__ = 'hiring_request'
+    request_id = db.Column(db.Integer, primary_key=True)  # Changed from Request_id
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.teacher_id'), nullable=False)  # Changed from Teacher_id
+    status = db.Column(db.Enum('pending', 'approved', 'rejected'), nullable=False)
+    request_date = db.Column(db.Date, nullable=False)
+    reviewed_by = db.Column(db.Integer, db.ForeignKey('admin.admin_id'))
+    reviewed_date = db.Column(db.Date)
