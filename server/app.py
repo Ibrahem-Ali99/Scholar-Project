bf7b98b5634ea59274ca188b4f02b02312f4c310
YusufBashaar <132370331+YusufBashaar@users.noreply.github.com>
2024-12-03 16:45:25 +0200

    Update app.py
    Fixed the landing page issue in app.py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from utils.db import db
-from routes.LandingPageCourses import course_bp
+from routes.LandingPageCourses import landing_course_bp
 from routes.teachers import teacher_bp  
 from routes.feedback import feedback_bp  
 from config import Config
@@ -31,6 +31,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 CORS(app)  
 mail = Mail(app)  
 db.init_app(app)  
+app.register_blueprint(landing_course_bp)
 app.register_blueprint(course_bp)
 app.register_blueprint(teacher_bp) 
 app.register_blueprint(feedback_bp) 
